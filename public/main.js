14d34ba562a3fd881743fa8b317214a396015c7d
RDiMag <87457132+RDiMag@users.noreply.github.com>
2023-01-19 13:07:08 -0500

    fixed the mime type mismatch issue, working

--- public/main.js
+++ public/main.js
@@ -1,1 +1,2 @@
-alert('working')
+alert('working'
+)
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+
+app.use(cors());
+app.use(express.static('public', options));
 
 const rappers = {
   '21 savage': {
@@ -24,15 +38,16 @@ app.get('/', (request, response)=>{
   response.sendFile(__dirname + '/index.html')
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
-  console.log(`The server is running on port ${PORT}! Better go catch it!`)
-})
-
 app.get('/api/:name', (request, response)=>{
   const rapperName = request.params.name.toLowerCase()
    if(rappers[rapperName]){
-    response.json(rappers[rapperName])
+    response.json (rappers[rapperName])
    } else {
     response.json(rappers['unknown'])
    }
+})
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+  console.log(`The server is running on port ${PORT}! Better go catch it!`)
 })
