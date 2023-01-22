ea25d5cd6b3e7ee612b7e52a2ff95c4259d1a627
RDiMag <87457132+RDiMag@users.noreply.github.com>
2023-01-22 11:56:36 -0500

    server set up with cyclic app url

--- public/main.js
+++ public/main.js
@@ -1,2 +1,14 @@
-console.log('working'
-)
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://tame-gold-centipede-ring.cyclic.app/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('p').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
